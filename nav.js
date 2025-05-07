f0d85b1eb87aa0fdacae3dbe8f1ff365e4ee8831
Lens D. <lensdorsainvil1@gmail.com>
2025-05-07 10:28:32 -0400

    created mobile nav

---
+++ nav.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('.menuIcon');
+const mobileMenu = document.querySelector('.mobileNav');
+
+
+
+menuIcon.addEventListener("click", () => {
+    mobileMenu.classList.toggle("revealNav");
+  });
+  
